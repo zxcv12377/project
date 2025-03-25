4653ca9017001d953e0b3e75d1a3f16031d71cbf
HyunWoo <mogs223@gmail.com>
2025-03-25 11:06:46 +0900

    KimsTest 파일 출력문 일부 수정

--- src/kims0711/KimsTest.java
+++ src/kims0711/KimsTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class KimsTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("숫자를 입력하시오 >> ");
+        System.out.print("숫자를 입력하시오 : "); // 출력문 변경
 
         String input = sc.nextLine();
         System.out.println("입력한 숫자 : " + input);
