81e71091abddc3f571227db8d890230684e656c8
ShimHyunJin <zxcv12377@naver.com>
2025-03-25 11:07:32 +0900

    이미지 추가 생성

--- src/sihyuntest/sihyuntest/test.java
+++
@@ -1,7 +0,0 @@
-package sihyuntest;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("test입니다.");
-    }
-}
---
+++ src/sihyuntest/sihyuntest1/test.java
@@ -0,0 +1,7 @@
+package sihyuntest.sihyuntest1;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("패키지 명이 같으면 안됩니다");
+    }
+}
