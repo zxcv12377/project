204e2d86a21c41c44cd36155698247961409e867
SEAN <shjung1994@hotmail.com>
2025-03-25 11:17:26 +0900

    TestEXE.java 추가

--- src/sihyuntest/sihyuntest/test.java
+++ src/sihyuntest/sihyuntest/test.java
@@ -1,7 +1,35 @@
 package sihyuntest;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("test입니다.");
+    
+    public String symbol;
+    int number;
+
+    int hour;
+    int minute;
+    float second;
+    
+    public int getHour() {
+        return hour;
+    }
+    public void setHour(int hour) {
+        if (hour < 0 || hour > 23) return;
+        this.hour = hour;
+    }
+    public int getMinute() {
+        return minute;
+    }
+    public void setMinute(int minute) {
+        if (minute < 0 || minute > 59)
+            return;
+        this.minute = minute;
+    }
+    public float getSecond() {
+        return second;
+    }
+    public void setSecond(float second) {
+        if (second < 0.0f || second > 59.9f)
+            return;
+        this.second = second;
     }
 }
